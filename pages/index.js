6da53acd58d87bdf166dc4606f7ca86320183103
Helmut Granda <contact@helmutgranda.com>
2021-01-27 22:35:27 -0600

    Adding another exclamation mark.

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>Space Jelly Shop</h1>
 
         <p className={styles.description}>
-          The best space jellyfish swag in the universe!
+          The best space jellyfish swag in the universe!!
         </p>
 
         <ul className={styles.grid}>
